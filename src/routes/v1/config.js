2fb3cfc4fa62eb2003b31e298d5bf00f6693f590
Ubaid Qureshi <ubaid@tesseract.in>
2021-05-09 21:37:39 +0530

    Basic Setup 2 - Mongoose/ Logger/ Request handler and Error handler

--- src/routes/v1/config.js
+++ src/routes/v1/config.js
@@ -5,6 +5,7 @@ const router = express.Router();
 const { version } = require('../../../package.json');
 
 router.get('/ping', (req, res) => {
+  _logger.info('PINGED');
   _handleResponse({
     res,
     statusCode: 200,
@@ -13,6 +14,7 @@ router.get('/ping', (req, res) => {
 });
 
 router.get('/version', (req, res) => {
+  _logger.info(`version - ${version}`);
   _handleResponse({
     res,
     statusCode: 200,
